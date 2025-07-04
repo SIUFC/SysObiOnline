5b2664726a339ebaf0baa38f607044f2db35965b
Pedro Alves <pedrosabiodos6caminhos@gmail.com>
2025-07-03 21:22:31 -0300

    add controller

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SysObiOnline.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
